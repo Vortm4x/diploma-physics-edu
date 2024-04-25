3e2d76af5db577216a53e3a63b100d1a7f69e6a9
Kalmarr-dev <art.hydrogen1405@gmail.com>
2024-04-25 18:03:45 +0300

    Create page with a header

---
+++ client/src/components/Header.vue
@@ -0,0 +1,45 @@
+<template>
+    <div id="header">
+        <div class="header-item">
+            <a class="header-link" href="#">Scenarios</a>
+        </div>
+        <div class="header-item">
+            <a class="header-link" href="#">Groups</a>
+        </div>
+        <div class="header-item">
+            <a class="header-link" href="#">My Profile</a>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style scoped>
+    #header {
+        height: 5em;
+        background-color: black;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: space-around;
+    }
+
+    .header-item {
+        vertical-align: middle;
+        flex: 2;
+    }
+
+    .header-link {
+        font-size: xx-large;
+        color: bisque;
+        text-decoration: none;
+    }
+
+    .header-link:hover {
+        text-decoration: underline;
+    }
+</style>
