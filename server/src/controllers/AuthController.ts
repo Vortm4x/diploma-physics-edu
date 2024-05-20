a0e62df4691b6db86b51298a3cfab36f2becb517
Kalmarr-dev <art.hydrogen1405@gmail.com>
2024-05-20 12:32:16 +0300

    Refactor controller

--- server/src/controllers/AuthController.ts
+++ server/src/controllers/AuthController.ts
@@ -4,15 +4,11 @@ import { db } from "../models"
 export default {
     async register(req: Request, res: Response): Promise<undefined> {
         try {
-            const user = db.User.create(req.body);
-            res.send((await user).toJSON());
+            const user = await db.User.create(req.body);
+            res.send(user.toJSON());
         } catch (error) {
             console.error(error);
             res.status(400).send(`User creation failed with email ${req.body.email}`);
         }
-        // res.send({
-        //     message: 'Your user was registered',
-        //     data: req.body
-        // })
     }
 }
